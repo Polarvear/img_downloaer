36110eeed0ce6565b66334f877365cf3c013f556
Polarvear <znzpzp13@naver.com>
2023-11-17 00:39:58 +0900

    전 부 탭 이미지 정보 가져오기 기능 진행중

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 // popup.js
-document.getElementById('downloadButton').addEventListener('click', function() {
+document
+  .getElementById("downloadButton")
+  .addEventListener("click", function () {
     chrome.runtime.sendMessage({ downloadImages: true });
     window.close(); // 팝업 창을 닫음
   });
--- scripts/content.js
+++ scripts/content.js
@@ -1,10 +1,8 @@
 // content.js
-window.addEventListener('load', function() {
-    const images = document.querySelectorAll('img');
-    const imageUrls = Array.from(images).map(img => img.src);
-    // console.log(imageUrls);
+window.addEventListener("load", function () {
+  const images = document.querySelectorAll("img");
+  const imageUrls = Array.from(images).map((img) => img.src);
+  // console.log(imageUrls);
 
-
-    chrome.runtime.sendMessage({ imageUrls: imageUrls });
-  });
-
+  chrome.runtime.sendMessage({ imageUrls: imageUrls });
+});
